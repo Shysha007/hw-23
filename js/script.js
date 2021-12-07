c1ed532cee58f259da37766e6570a73f89762b9d
Ольга <91964782+Shysha007@users.noreply.github.com>
2021-12-08 01:20:41 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function count(num = 0) {
+
+    return function (numTwo) {
+        return num += numTwo;
+    }
+}
+
+let sum = count();
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
